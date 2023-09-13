81766a70850068c82808a3b925a924aef69df3b7
billysistian <billy.sistianputra@gmail.com>
2023-09-13 16:31:14 +0700

    update

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -3,32 +3,38 @@ import { Link } from "react-router-dom";
 import { DarkMode } from "../../context/DarkMode";
 
 const AuthLayouts = (props) => {
-    const {children, title, type} = props;
-    const {isDarkMode, setIsDarkMode} = useContext(DarkMode);
-    {
-      console.log(isDarkMode)
-    }
-    return (
-    <div className={`flex justify-center min-h-screen items-center ${isDarkMode && "bg-slate-900"}`}>
-    <div className="w-full max-w-xs rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
-      <button
-        className="absolute right-2 top-2 bg-blue-600 p-2 text-white rounded" 
-        onClick={() => setIsDarkMode(!isDarkMode)}
-      >
-        {isDarkMode ? "Light" : "Dark"}
-      </button>
-      <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
-      <p className="font-medium text-slate-500 mb-8">
-        Welcome, please enter your details
-      </p>
-      {children}
-      <Auth type={type} />
+  const { children, title, type } = props;
+  const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
+  {
+    console.log(isDarkMode);
+  }
+  return (
+    <div
+      className={`flex justify-center min-h-screen items-center ${
+        isDarkMode && "bg-slate-900"
+      }`}
+    >
+      <div className="w-full max-w-xs rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
+        <div className="absolute right-2 top-2 p-2">
+          <input
+            className="ml-8 mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+            type="checkbox"
+            role="switch"
+            onClick={() => setIsDarkMode(!isDarkMode)}
+          />
+        </div>
+        <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Welcome, please enter your details
+        </p>
+        {children}
+        <Auth type={type} />
+      </div>
     </div>
-    </div>
-    );
+  );
 };
 
-const Auth = ({type}) => {
+const Auth = ({ type }) => {
   if (type === "login") {
     return (
       <p className="text-sm mt-5 text-center">
@@ -48,6 +54,6 @@ const Auth = ({type}) => {
       </p>
     );
   }
-}
+};
 
 export default AuthLayouts;
